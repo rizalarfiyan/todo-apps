57f944ffc5b294ee1553c1286ee028cbb7784930
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-29 23:37:26 +0700

    feat: add tailwindcss, sass, postcss, autoprefixer

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='wrapper'>
-      <h1>Hello World</h1>
+    <div className='flex min-h-screen w-full items-center justify-center'>
+      <h1 className='text-4xl font-semibold text-gray-800'>Todo Apps</h1>
     </div>
   )
 }

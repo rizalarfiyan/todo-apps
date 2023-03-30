0c8930981f597994d6cfe8ccf576ce53d980ce18
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-30 23:00:34 +0700

    feat: add empty illustration

--- src/features/activity/components/Activity/Page.tsx
+++ src/features/activity/components/Activity/Page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import Button from '@components/Button'
 import Icon from '@components/Icon'
 
+import Empty from './Empty'
+
 const Page = () => {
   return (
-    <div className='container'>
+    <div className='container space-y-20'>
       <div className='flex items-center justify-between gap-3'>
         <h1 className='text-4xl font-semibold text-gray-800'>Activity</h1>
         <Button
@@ -19,6 +21,7 @@ const Page = () => {
           Add
         </Button>
       </div>
+      <Empty className='mx-auto h-auto w-full max-w-xl' />
     </div>
   )
 }
--- src/features/activity/components/DetailActivity/Page.tsx
+++ src/features/activity/components/DetailActivity/Page.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
+import Button from '@components/Button'
+import Icon from '@components/Icon'
+
+import Empty from './Empty'
+
 const Page = () => {
   return (
-    <div className='flex min-h-screen w-full items-center justify-center'>
-      <h1 className='text-4xl font-semibold text-gray-800'>
-        Detail Activity Pages
-      </h1>
+    <div className='container space-y-20'>
+      <div className='flex items-center justify-between gap-3'>
+        <h1 className='text-4xl font-semibold text-gray-800'>New Activity</h1>
+        <Button
+          type='button'
+          leftIcon={<Icon type='plus' className='mr-2 h-5 w-5' />}
+          variant='solid'
+          color='primary'
+          size='lg'
+          isRounded
+        >
+          Add
+        </Button>
+      </div>
+      <Empty className='mx-auto h-auto w-full max-w-xl' />
     </div>
   )
 }

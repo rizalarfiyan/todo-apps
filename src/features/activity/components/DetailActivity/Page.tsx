0d25edfb4e580f69d2a25593f5626701bd8d960b
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-04-01 01:07:17 +0700

    feat: add content title in detail activity

--- src/features/activity/components/DetailActivity/Page.tsx
+++ src/features/activity/components/DetailActivity/Page.tsx
@@ -44,19 +44,56 @@ const Page = () => {
     <div className='container space-y-20'>
       <TodoModal modal={modal} />
       <div className='flex items-center justify-between gap-3'>
-        <h2 className='text-4xl font-semibold text-gray-800'>New Activity</h2>
-        <Button
-          type='button'
-          leftIcon={<Icon type='plus' className='mr-2 h-5 w-5' />}
-          variant='solid'
-          color='primary'
-          size='lg'
-          disabled={activityDetail.isLoading}
-          onClick={handleCreateTodo}
-          isRounded
-        >
-          Tambah
-        </Button>
+        <div className='flex w-full items-center gap-3 text-gray-800'>
+          <Link to='/'>
+            <Icon type='back' className='h-8 w-8' />
+          </Link>
+          {activityDetail.isLoading ? (
+            <div className='flex w-full max-w-3xl flex-col gap-2.5'>
+              <div className='h-6 w-full animate-pulse rounded-md bg-gray-200' />
+              <div className='h-6 w-full animate-pulse rounded-md bg-gray-200' />
+              <div className='h-6 w-1/2 animate-pulse rounded-md bg-gray-200' />
+            </div>
+          ) : (
+            <h2 className='line-clamp-3 max-w-3xl text-2xl font-semibold'>
+              {activityDetail.data?.title}
+            </h2>
+          )}
+          <Button
+            type='button'
+            variant='ghost'
+            isIcon
+            isRounded
+            disabled={activityDetail.isLoading}
+          >
+            <Icon type='pencil' className='h-5 w-5 text-gray-600' />
+          </Button>
+        </div>
+        <div className='flex items-center gap-4'>
+          <Button
+            type='button'
+            variant='solid'
+            className='!px-3'
+            size='lg'
+            isIcon
+            disabled={activityDetail.isLoading}
+            isRounded
+          >
+            <Icon type='sort' className='h-5 w-5 text-gray-600' />
+          </Button>
+          <Button
+            type='button'
+            leftIcon={<Icon type='plus' className='mr-2 h-5 w-5' />}
+            variant='solid'
+            color='primary'
+            size='lg'
+            disabled={activityDetail.isLoading}
+            onClick={handleCreateTodo}
+            isRounded
+          >
+            Tambah
+          </Button>
+        </div>
       </div>
       <Empty className='mx-auto h-auto w-full max-w-xl' />
     </div>
